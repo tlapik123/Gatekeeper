88c7c433d5a58831b7ea1ae79b351c1d8e106265
Petr Tlapa <tlapik123@gmail.com>
2022-08-14 15:11:30 +0200

    Cleaned up the interaction loop for reaction command.

--- GkModules/GkPermsModule.cs
+++ GkModules/GkPermsModule.cs
@@ -1,7 +1,9 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using System.Globalization;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.CommandsNext.Converters;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using gatekeeper.GkPermsHandler;
 
@@ -11,11 +13,8 @@ namespace gatekeeper.GkModules;
 /// Houses commands regarding permission management.
 /// </summary>
 public class GkPermsModule : BaseCommandModule {
-    private readonly IArgumentConverter<DiscordMessage> _messageConverter;
-    private readonly IArgumentConverter<DiscordEmoji> _emojiConverter;
-    private readonly IArgumentConverter<DiscordChannel> _channelConverter;
-    private readonly IArgumentConverter<bool> _boolConverter;
     private readonly IGkReactionPermsDatabase _reactionPermsDatabase;
+    private readonly IDictionary<Type, IArgumentConverter> _convertors;
 
     public GkPermsModule(
         IArgumentConverter<DiscordMessage> messageConverter,
@@ -24,11 +23,13 @@ public class GkPermsModule : BaseCommandModule {
         IArgumentConverter<bool> boolConverter,
         IGkReactionPermsDatabase reactionPermsDatabase
     ) {
-        _messageConverter = messageConverter;
-        _emojiConverter = emojiConverter;
-        _channelConverter = channelConverter;
-        _boolConverter = boolConverter;
         _reactionPermsDatabase = reactionPermsDatabase;
+        _convertors = new Dictionary<Type, IArgumentConverter> {
+            { messageConverter.GetType(), messageConverter },
+            { emojiConverter.GetType(), emojiConverter },
+            { channelConverter.GetType(), channelConverter },
+            { boolConverter.GetType(), boolConverter },
+        };
     }
 
     [Command("react_to_perms")]
@@ -42,55 +43,56 @@ public class GkPermsModule : BaseCommandModule {
 What message will users be reacting to? (Enter valid message ID)"
         );
 
-        DiscordMessage? message = null;
         Dictionary<DiscordEmoji, DiscordChannel> changeDict = new();
 
         // TODO: let the user end in any moment?
-
-        var chain = (await interactivity.WaitForMessageAsync(m => {
-            var taskRes = _messageConverter.ConvertAsync(m.Content, ctx).Result;
-            if (!taskRes.HasValue) return false;
-            message = taskRes.Value;
-            return true;
-        })).Result;
-
-
-        while (true) {
-            DiscordEmoji? tmpEmoji = null;
-
-            await chain.RespondAsync("What emoji will they use? (Enter an emoji)");
-            chain = (await interactivity.WaitForMessageAsync(m => {
-                var taskRes = _emojiConverter.ConvertAsync(m.Content, ctx).Result;
-                if (!taskRes.HasValue) return false;
-                tmpEmoji = taskRes.Value;
-                return true;
-            })).Result;
-
-            DiscordChannel? tmpChannel = null;
-
-            await chain.RespondAsync("What channel will they have access to? (Enter a channel)");
-            chain = (await interactivity.WaitForMessageAsync(m => {
-                var taskRes = _channelConverter.ConvertAsync(m.Content, ctx).Result;
-                if (!taskRes.HasValue) return false;
-                tmpChannel = taskRes.Value;
-                return true;
-            })).Result;
-
-            // TODO: null when timeout
-            changeDict[tmpEmoji] = tmpChannel;
-
-            await chain.RespondAsync("Specify more emoji -> channel pairs?");
-            chain = (await interactivity.WaitForMessageAsync(_ => true)).Result;
-            if (await _boolConverter.ConvertAsync(chain.Content, ctx) is { HasValue: true, Value: false, }) break;
+        if (await TryGetResponseAsync<DiscordMessage>(interactivity, ctx) is not { } mRes) {
+            await TimedOutRespondAsync(ctx.Message);
+            return;
         }
 
+        var (messageChain, message) = mRes;
+        // Get the emoji-channel pairs
+        while (true) {
+            await messageChain.RespondAsync("What emoji will they use? (Enter an emoji)");
+            if (await TryGetResponseAsync<DiscordEmoji>(interactivity, ctx) is not { } eRes) {
+                await TimedOutRespondAsync(messageChain);
+                return;
+            }
+
+            (messageChain, var tmpEmoji) = eRes;
+
+            await messageChain.RespondAsync("What channel will they have access to? (Enter a channel)");
+            if (await TryGetResponseAsync<DiscordChannel>(interactivity, ctx) is not { } cRes) {
+                await TimedOutRespondAsync(messageChain);
+                return;
+            }
+
+            (messageChain, var tmpChannel) = cRes;
+
+            changeDict[tmpEmoji] = tmpChannel;
+
+            await messageChain.RespondAsync("Specify more emoji -> channel pairs?");
+            if (await TryGetResponseAsync<bool>(interactivity, ctx) is not { } bRes) {
+                await TimedOutRespondAsync(messageChain);
+                return;
+            }
+
+            (messageChain, var tmpBool) = bRes;
+            if (!tmpBool) {
+                await messageChain.RespondAsync("OK! No more emoji channel pairs!");
+                break;
+            }
+        }
+        // add binding to database
         await _reactionPermsDatabase.AddOrUpdateChannel(message.Id, changeDict);
+
         // indicate user with reaction to the message from bot and with message
         foreach (var emoji in changeDict.Keys) {
             await message.CreateReactionAsync(emoji);
         }
 
-        await chain.RespondAsync("All set!");
+        await messageChain.RespondAsync("All set!");
     }
 
     [Command("react_to_perms_c")]
@@ -111,5 +113,56 @@ What message will users be reacting to? (Enter valid message ID)"
         }
 
         await ctx.RespondAsync("All set!");
+    }
+
+    /// <summary>
+    /// Tries to get response from the user convertable to given Type. 
+    /// </summary>
+    /// <param name="interactivity">Interactivity</param>
+    /// <param name="ctx">Command context</param>
+    /// <typeparam name="T">
+    /// Only response convertible to this Type will be accepted.
+    /// </typeparam>
+    /// <returns>
+    /// Tuple of DiscordMessage and Type result - if the response didn't time out.
+    /// Null - response timed out.
+    /// </returns>
+    private async Task<(DiscordMessage, T)?> TryGetResponseAsync<T>(InteractivityExtension interactivity,
+        CommandContext ctx) {
+        T? retType = default;
+        // get correct convertor
+        if (TryGetConvertor<T>() is not { } convertor) return null;
+
+        // chain is null only if TimedOut is true.
+        var interactivityResult = await interactivity.WaitForMessageAsync(m => {
+            var taskRes = convertor.ConvertAsync(m.Content, ctx).Result;
+            if (!taskRes.HasValue) return false;
+            retType = taskRes.Value;
+            return true;
+        });
+        if (interactivityResult.TimedOut || retType is null) return null;
+
+        return (interactivityResult.Result, retType);
+    }
+
+    /// <summary>
+    /// Tries to get convertor from available convertors that were given to this module.
+    /// </summary>
+    /// <typeparam name="T">Type of thing that the convertor will convert. (IArgumentConverter of T)</typeparam>
+    /// <returns>
+    /// IArgumentConverter of T - a convertor that can convert to type T. 
+    /// </returns>
+    private IArgumentConverter<T>? TryGetConvertor<T>() {
+        return _convertors.TryGetValue(typeof(IArgumentConverter<T>), out var convertor)
+            ? (IArgumentConverter<T>)convertor
+            : null;
+    }
+
+    /// <summary>
+    /// Inform user about the time out.
+    /// </summary>
+    /// <param name="message">Message to respond to.</param>
+    private static async Task TimedOutRespondAsync(DiscordMessage message) {
+        await message.RespondAsync("No valid response was given, timed-out! Closing the interactivity!");
     }
 }
